0b280131062ee5050d4be5ab078121fc3b6d7396
JJDocs <animegamingdoc@gmail.com>
2022-07-08 01:12:20 -0500

    Test Commit to new github

--- app/src/main/java/com/example/finalproject/Favourites_activity.java
+++ app/src/main/java/com/example/finalproject/Favourites_activity.java
@@ -21,6 +21,6 @@ public class Favourites_activity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourites_activity, container, false)
+        return inflater.inflate(R.layout.fragment_favourites_activity, container, false);
     }
 }
